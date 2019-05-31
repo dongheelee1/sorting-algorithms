a8979ac16a6485ede2dc5f0095070b0512bf2d17
donghizzle <dongheec.lee@gmail.com>
2019-05-30 23:01:53 -0400

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,1 +1,28 @@
+'''
+IDEA: 
 
+// Find the minimum element in arr[0...4]
+// and place it at beginning
+
+// Find the minimum element in arr[1...4]
+// and place it at beginning of arr[1...4]
+
+// Find the minimum element in arr[2...4]
+// and place it at beginning of arr[2...4] 
+
+// Find the minimum element in arr[3...4]
+// and place it at beginning of arr[3...4]
+'''
+
+
+
+for i in range(0, len(arr)): 
+  
+  min = i 
+  
+  for j in range(i+1, len(arr)): 
+    if arr[min] > arr[j]: 
+      min = j 
+      
+  arr[i], arr[min] = arr[min], arr[i] 
+      
